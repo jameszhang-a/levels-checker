ddd3bb95871550ef6e8fa47f897b0688a7f0601d
James Zhang <jameszhanga@gmail.com>
2022-04-19 19:07:59 -0500

    Pop up able to send message to backend

--- src/background.js
+++ src/background.js
@@ -22,5 +22,6 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   // 2. A page requested user data, respond with a copy of `user`
   if (message === 'get-url') {
     sendResponse(url);
+    console.log('got message', url);
   }
 });
