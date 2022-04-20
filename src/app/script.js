0f9bf241389ad5bec51c448c124f5a2f52bee2b8
James Zhang <jameszhanga@gmail.com>
2022-04-19 19:50:01 -0500

    Able to open company tab

--- src/app/script.js
+++ src/app/script.js
@@ -19,16 +19,24 @@ async function fetchData() {
   if (payload === 'I9DW+hnEyOhcwFdtrpUWEA==') {
     document.getElementById('result').innerHTML = 'Not available';
   } else {
-    document.getElementById(
-      'result'
-    ).innerHTML = `<a href='https://www.levels.fyi/company/Amazon'>${company}</a>`;
+    // document.getElementById(
+    //   'result'
+    // ).innerHTML = `<a href='https://www.levels.fyi/company/Amazon'>${company}</a>`;
+    let anchor = document.createElement('a');
+    let link = document.createTextNode(`Check out ${company}`);
+    anchor.appendChild(link);
+    anchor.href = `https://www.levels.fyi/comp.html?track=Software%20Engineer&search=${company}`;
+    anchor.target = '_blank';
+    chrome.tabs.create({ url: 'http://www.google.com', active: false });
+
+    document.getElementById('container').appendChild(anchor);
   }
 }
-fetchData();
 
 chrome.runtime.sendMessage('get-url', (res) => {
   // 3. Got an asynchronous response with the data from the background
   console.log('received user data', res);
   url = res;
   document.getElementById('url').innerHTML = `${url}`;
+  fetchData();
 });
